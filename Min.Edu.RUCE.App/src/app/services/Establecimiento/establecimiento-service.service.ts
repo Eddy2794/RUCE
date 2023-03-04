3f09977aec7235a8934497720ce48eb319198297
Eddy2794 <67075436+Eddy2794@users.noreply.github.com>
2023-03-03 22:41:22 -0300

    Configuración de los servicios del FrontEnd Se agregó los servicios necesarios y los métodos GET y POST para el crud de Establecimientos Educativos

--- Min.Edu.RUCE.App/src/app/services/Establecimiento/establecimiento-service.service.ts
+++ Min.Edu.RUCE.App/src/app/services/Establecimiento/establecimiento-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EstablecimientoModel } from 'src/app/models/Establecimiento/establecimiento-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstablecimientoServiceService {
 
-  constructor() { }
+  baseUrl = 'http://127.0.0.1:8000/api/establecimiento_educativo';
+
+  constructor(private http: HttpClient) { }
+
+  public all(): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl , httpOptions);
+  }
+
+  public createEstablecimiento(establecimiento: EstablecimientoModel): Observable<any> {
+    const httpOptions = {
+      method: 'POST',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(establecimiento);
+    return this.http.post(this.baseUrl, body, httpOptions);
+  }
+  
 }
