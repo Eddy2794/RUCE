1576ab0844655277c6453588d0b8b9aedf2e7478
Eddy2794 <67075436+Eddy2794@users.noreply.github.com>
2023-03-04 21:46:10 -0300

    Configuración de los servicios del FrontEnd Definición de las funciones de los servicios para primer CRUD

--- Min.Edu.RUCE.App/src/app/services/Persona/persona.service.ts
+++ Min.Edu.RUCE.App/src/app/services/Persona/persona.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PersonaModel } from 'src/app/models/Persona/persona-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
-  constructor() { }
+  baseUrl = 'http://127.0.0.1:8000/api/establecimiento_educativo';
+
+  constructor(private http: HttpClient) { }
+
+  public all(): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl , httpOptions);
+  }
+
+  public create(persona: PersonaModel): Observable<any> {
+    const httpOptions = {
+      method: 'POST',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(persona);
+    return this.http.post(this.baseUrl, body, httpOptions);
+  }
+
+  public get(id: number): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl + id, httpOptions);
+  }
+
+  public update(id: number, persona: PersonaModel) {
+    const httpOptions = {
+      method: 'PUT',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(persona);
+    return this.http.put(this.baseUrl + id, body, httpOptions);
+  }
+
+  public delete(id: number) {
+    const httpOptions = {
+      method: 'DELETE',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.delete(this.baseUrl + id, httpOptions);
+  }
 }
