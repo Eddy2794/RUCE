7e50dac83b767bece7c0b3e212232cb547fb3053
Eddy2794 <67075436+Eddy2794@users.noreply.github.com>
2023-03-04 22:34:33 -0300

    Configuración de los servicios del FrontEnd Definición de las funciones de todos los services

--- Min.Edu.RUCE.App/src/app/services/Cooperadora_TipoAsociacion/cooperadora-tipo-asociacion.service.ts
+++ Min.Edu.RUCE.App/src/app/services/Cooperadora_TipoAsociacion/cooperadora-tipo-asociacion.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CooperadoraTipoAsociacionModel } from 'src/app/models/CooperadoraTipoAsociacion/cooperadora-tipo-asociacion-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CooperadoraTipoAsociacionService {
 
-  constructor() { }
+  baseUrl = 'http://127.0.0.1:8000/api/coop_tipo_asociacion';
+
+  constructor(private http: HttpClient) { }
+
+  public all(): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl , httpOptions);
+  }
+
+  public create(coopTipoAsociacion: CooperadoraTipoAsociacionModel): Observable<any> {
+    const httpOptions = {
+      method: 'POST',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(coopTipoAsociacion);
+    return this.http.post(this.baseUrl, body, httpOptions);
+  }
+
+  public get(id: number): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl + id, httpOptions);
+  }
+
+  public update(id: number, coopTipoAsociacion: CooperadoraTipoAsociacionModel) {
+    const httpOptions = {
+      method: 'PUT',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(coopTipoAsociacion);
+    return this.http.put(this.baseUrl + id, body, httpOptions);
+  }
+
+  public delete(id: number) {
+    const httpOptions = {
+      method: 'DELETE',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.delete(this.baseUrl + id, httpOptions);
+  }
 }
