b8bc67e21a0826fb988f8cdccf5ef860694a8932
Eddy2794 <mamaniedgardo20@gmail.com>
2024-10-28 12:34:52 -0300

    Reportes generado en el back, para configurar

--- Min.Edu.RUCE.App/src/app/pages/ruce/reportes/Components/reportes-list/reportes-list.component.ts
+++ Min.Edu.RUCE.App/src/app/pages/ruce/reportes/Components/reportes-list/reportes-list.component.ts
@@ -224,19 +224,4 @@ export class ReportesListComponent implements OnInit {
     });
   }
 
-  exportToExcel(): void {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.columnasVex.map(col => {
-      return {
-        label: col.label,
-        property: col.property,
-        type: col.type,
-        visible: col.visible
-      };
-    }));
-
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Reportes');
-    XLSX.writeFile(wb, 'reportes.xlsx');
-  }
-
 }
